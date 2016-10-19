afb8954875161f1eb5ecd5b029c1f97ddda4a573
Mario <Mario>
2016-10-19 18:11:21 -0400

    attempt at finishing main.js, still missing functions on word.js

--- main.js
+++ main.js
@@ -6,11 +6,12 @@ var Game = require('./game.js');
 
 prompt.start();
 
-game = {
-  wordBank : game.newWord.wordList, // create or import a list of words
+var game = {
+  wordBank : Game.newWord.wordList, // create or import a list of words
   wordsWon : 0,// count of words Found
   guessesRemaining : 10, //per word
   currentWrd : null, //the word object
+  guessedLetters : [],
   startGame : function (wrd){
     //make sure the user has 10 guesses
     //get a random word from the array
@@ -18,7 +19,7 @@ game = {
     if(this.guessesRemaining == 10){
       var randomNumber = Math.floor(Math.random() * this.wordBank.length);
       this.currentWrd = new Word[this.wordBank[randomNumber]];
-      this.currentWord.getLets();
+      this.currentWrd.getLets();
       //displays current word as blanks.
       console.log(this.currentWord.wordRender());
       this.keepPromptingUser();
@@ -36,7 +37,7 @@ game = {
     var self = this;
 
     inquirer.prompt([{
-      name: "choseLetter",
+      name: "chosenLetter",
       type: "input",
       message: "Choose a letter:",
       validate: function(value) {
@@ -46,36 +47,42 @@ game = {
           return false;
         }
       }
-    }].then(function(answer) {
-       
-        
+    }]).then(function(answer) {
+
         // console log the letetr you chose
-
+        console.log(answer.chosenLetter);
         //this checks if the letter was found and if it is then it sets that specific letter in the word to be found
+        that.guessedLetters.push(answer.chosenLetter);
 
+        var found = self.currentWrd.checkIfLetterFound(answer.chosenLetter);
         //if the user guessed incorrectly minus the number of guesses they have left
         // and console.log if they were incorrect or correct
-          
-        //check if you win only when you are right
-        //end game
-       
-        
-        // display the user how many guesses remaining
-      
-        // render the word 
-        
-        // display letters the user has guessed
+          if(found === 0){
+            console.log('Your guess is wrong.');
+            self.guessesRemaining--;
+            console.log('Guesses remaining: ' + self.guessesRemaining);
 
-        // if user has remaining guesses and Word isn't found
-      
-        // if user has no guesses left, show them the word and tell them they lost
-      
-        // else show the user word and rendered
-        
+            console.log("Letters guessed: " + that.guessedLetters);
+          } else{
+            console.log('Your guess is right!');
+              //checks to see if user won
+              if(that.currentWord.didWeFindTheWord() == true){
+                console.log(that.currentWord.wordRender());
+                console.log('You won the game!!!');
+              } else{
+                // display the user how many guesses remaining
+                console.log('Guesses remaining: ' + that.guessesRemaining);
+                console.log("Letters guessed: " + that.guessedLetters);
+              }
+          }
+          if(that.guessesRemaining > 0 && that.currentWord.wordFound === false) {
+            that.keepPromptingUser();
+          }else if(that.guessesRemaining === 0){
+            console.log('Game over!');
+            console.log('The word you were guessing was: ' + that.currentWord.word);
+          }
     });
   }
 
-
-};
 
 game.startGame();
