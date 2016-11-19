e84b3c227360e14cd89ceecdf50d85ea8ae8c17d
jmona789 <jmona789@gmail.com>
2016-11-19 14:46:27 -0500

    removed semicolon causing error

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 exports.newWord = {
-  wordList: ["airplane", "boat", "train"];
+  //removed semicolon causing syntax error- when making a JS object semicolons at the end of lines with cause errors
+  wordList: ["airplane", "boat", "train"]
 };
 // creates a word list to use in main.js
